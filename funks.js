06eb288e0b238c7809ae42c0db265ec006504a18
VitorPrates <vitoraaprates@gmail.com>
2026-05-13 22:10:04 -0300

    adicionando login ao site

--- funks.js
+++ funks.js
@@ -1,4 +1,54 @@
+const form_login = document.querySelector(".form_login")
+const texto_user_logado = document.querySelector('.nome_user')
+let user_logado
+
 function alertar_sobre(nome)
 {
     alert(`Ver sobre: ${nome}`)
+}
+
+window.addEventListener('load', () => {
+    texto_user_logado.innerHTML = user_logado
+})
+
+form_login.addEventListener('submit', (eve) =>{
+    eve.preventDefault()
+    consultarLogin()
+})
+
+async function consultarLogin() {
+    const senha = document.getElementById('senha').value;
+    const email = document.getElementById('email').value;
+    const pesquisa = document.getElementById('resultado')
+    
+    pesquisa.innerHTML = "Perai..."
+    console.log(senha);
+    console.log(email);
+
+    user_logado = email
+    window.location.href = "dashboard.html"
+    
+    // const url =
+    // `https://script.google.com/macros/s/AKfycbyFJxCgEh2KvnvrHqmUvrT0uaAOR10XMXLNdViHfmRvzXSd2w1nVS10qyyNcmVFAN47Pg/exec?id=${encodeURIComponent(id)}`;
+
+    // try {
+    //     const response = await fetch(url);
+    //     const dados = await response.json();
+
+    //     const senha_encontrada = dados.senha
+    //     const email_encontrado = dados.email
+
+    //     // resultado.innerHTML = `Encontrado: ${dados.quantidade}`
+
+    //     if(senha == senha_encontrada && email == email_encontrado)
+    //     {
+    //         alert("dale")
+    //     }
+    //     console.log(dados);
+
+    // } catch (err) {
+    //     resultado.innerHTML = "não encontrado"
+    //     console.error(err);
+    // }
+    
 }
