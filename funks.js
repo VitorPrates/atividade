81a7fc53a41000f06b0aa2bc18ccefd5e71ac823
VitorPrates <vitoraaprates@gmail.com>
2026-05-14 22:14:02 -0300

    adicionado o módulo de pesquisa de bombas

--- funks.js
+++ funks.js
@@ -2,6 +2,33 @@ const form_login = document.querySelector(".form_login")
 const texto_user_logado = document.querySelector('.nome_user')
 let user_logado
 
+//dashboard coisas
+const cards_dashboard = document.querySelectorAll(".card_default_dashboard")
+const btns_dashboard = document.querySelectorAll(".btn_dashboard")
+
+btns_dashboard.forEach((e) => {
+    e.addEventListener('click', () => {
+        switch (e.innerHTML) {
+            case "Pagina inicial":
+                cards_dashboard.forEach((eve) => {
+                    eve.style.top = "100%"
+                })
+                cards_dashboard[0].style.top = "40%"
+                break;
+            case "Consultar Produto":
+                cards_dashboard.forEach((eve) => {
+                    eve.style.top = "100%"
+                })
+                cards_dashboard[1].style.top = "10%"
+                break;
+        
+            default:
+                break;
+        }
+        
+    })
+})
+
 function alertar_sobre(nome)
 {
     alert(`Ver sobre: ${nome}`)
@@ -61,6 +88,43 @@ async function consultarLogin() {
                 window.location.href = "dashboard.html"
             }, 1500);
             // alert("dale")
+        }
+        console.log(dados);
+
+    } catch (err) {
+        resultado.innerHTML = "não encontrado"
+        console.error(err);
+    }
+}
+
+async function buscar_produto() {
+    const item_pesquisa = document.getElementById('id_item_pesquisa').value;
+    const imagem_resultado_item = document.getElementById('imagem_resultado_item');
+    const nome_resultado_item = document.getElementById('nome_resultado_item');
+    const descri_resultado_item = document.getElementById('descri_resultado_item');
+    const preco_resultado_item = document.getElementById('preco_resultado_item');
+    const resultado_pesquisa_feedback = document.getElementById('resultado_pesquisa');
+    
+    // window.location.href = "dashboard.html"
+
+    resultado_pesquisa_feedback.innerHTML = "Pesquisando...."
+    
+    const url =`https://script.google.com/macros/s/AKfycbyFJxCgEh2KvnvrHqmUvrT0uaAOR10XMXLNdViHfmRvzXSd2w1nVS10qyyNcmVFAN47Pg/exec?id_item=${encodeURIComponent(item_pesquisa)}`;
+
+    try {
+        const response = await fetch(url);
+        const dados = await response.json();
+        if(dados.status == "nao_encontrado")
+        {
+            resultado_pesquisa_feedback.innerHTML = "Não encontrado :("
+        }
+        else
+        {
+            resultado_pesquisa_feedback.innerHTML = ""
+            imagem_resultado_item.src = dados.imagem
+            nome_resultado_item.innerText = dados.nome
+            descri_resultado_item.innerText = dados.descricao
+            preco_resultado_item.innerText = `${dados.preco.toLocaleString("pt-BR",{style:"currency", currency:"BRL"})}`
         }
         console.log(dados);
 
